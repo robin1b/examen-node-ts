edd9242da68a199c6027bea88584eeedff37368e
robin <robin1broos@gmail.com>
2025-03-12 11:52:46 +0100

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,34 @@
-// Imports
-import "dotenv/config";
-import cors from "cors";
 import express from "express";
-import { notFound } from "./controllers/notFoundController";
-import testRoutes from "./routes/snippetRoutes";
-import { helloMiddleware } from "./middleware/errorHandler";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import snippetRoutes from "./routes/snippetRoutes";
+import Snippet, { ISnippet } from "./models/Snippet";
+dotenv.config();
 
-// Variables
+mongoose.connect(process.env.MONGODB_URI!);
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-// Routes
-app.use("/api", helloMiddleware, testRoutes);
-app.all("*", notFound);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
-// Server Listening
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}! 🚀`);
+app.use("/api", snippetRoutes);
+
+app.get("/", async (_, res) => {
+  const snippets = (await Snippet.find().lean()) as ISnippet[];
+
+  snippets.forEach((snippet: ISnippet) => {
+    snippet.code = Buffer.from(snippet.code, "base64").toString("utf-8");
+  });
+
+  res.render("index", { snippets });
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
