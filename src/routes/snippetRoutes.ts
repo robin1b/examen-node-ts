ad8ebdaf4695dac59890caa50d7ecd033ac812e9
robin <robin1broos@gmail.com>
2025-03-12 11:12:27 +0100

    snippetrouter

--- src/routes/snippetRoutes.ts
+++ src/routes/snippetRoutes.ts
@@ -1,5 +1,13 @@
 import express from "express";
-
+import {
+  createSnippet,
+  getSnippetById,
+  deleteSnippet,
+} from "../controllers/snippetController";
 const router = express.Router();
+
+router.post("/snippets", createSnippet);
+router.get("/snippets/:id", getSnippetById);
+router.delete("/snippets/:id", deleteSnippet);
 
 export default router;
