fd70a9af09d322a69b16db16aed879dc101e77fc
robin <robin1broos@gmail.com>
2025-03-12 10:43:46 +0100

    interface controller

--- src/controllers/snippetController.ts
+++ src/controllers/snippetController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
+export interface Isnippet extends Document {
+  title: string;
+  code: string;
+  language: string;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
 export const getHelloWorld = (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello World!" });
 };
